3ea5fa8d0365c8c530e2f2b9beadada24a5e20ff
Peter Öhlinger <peteroehlinger@Peters-MacBook-Pro.local>
2024-08-04 15:10:32 +0200

    Create three plot structure (goods market, islm, money market) and move parameters inputs to 4th panel.

--- server/islmServer.R
+++ server/islmServer.R
@@ -4,22 +4,53 @@
 #
 ###############################################
 
-output$islmPlot <- renderPlot({
+output$goodsMarketPlot <- renderPlot({
   
   plot <- ggplot() +
     geom_abline(slope = 1) +
     geom_abline(intercept = input$c_0_islm, 
                 slope = input$c_1_islm) +
     scale_y_continuous(limits = c(0, 2000), expand = c(0,0)) +
-    scale_x_continuous(limits = c(0, 2000), expand = c(0,0))
+    scale_x_continuous(limits = c(0, 2000), expand = c(0,50))
   
   gx_theme(plot)
 
 })
 
+output$interestRatePlot <- renderPlot({
+  
+  plot <- ggplot() +
+    geom_hline(yintercept = input$i_islm) +
+    scale_y_continuous(limits = c(-0.05, 0.5), expand = c(0,0)) +
+    scale_x_continuous(limits = c(0, 2000), expand = c(0,50))
+  
+  gx_theme(plot)
+  
+})
+
+output$islmPlot <- renderPlot({
+  
+  plot <- ggplot() +
+    geom_hline(yintercept = input$i_islm) +
+    scale_y_continuous(limits = c(-0.05, 0.5), expand = c(0,0)) +
+    scale_x_continuous(limits = c(0, 2000), expand = c(0,50))
+  
+  gx_theme(plot)
+  
+})
+
 output$islmEquilibrium <- renderText({paste("The equilibrium is", input$c_0_islm/(1-input$c_1_islm))})
 
 # input validation
-# iv <- InputValidator$new
-# iv$add_rule("c_1_islm", sv_between(0.01, 0.99))
-# iv$enable
+iv <- InputValidator$new()
+iv$add_rule("c_0_islm", sv_between(50, 500))
+iv$add_rule("c_1_islm", sv_between(0.01, 0.99))
+iv$add_rule("I_0_islm", sv_between(0, 500))
+iv$add_rule("I_1_islm", sv_between(0.01, 0.99))
+iv$add_rule("I_2_islm", sv_between(0, 10000))
+
+
+iv$add_rule("G_islm", sv_between(0, 500))
+iv$add_rule("T_islm", sv_between(0, 500))
+iv$add_rule("i_islm", sv_between(-0.01, 0.5))
+iv$enable()
--- ui/islmUI.R
+++ ui/islmUI.R
@@ -7,37 +7,52 @@
 islmTab <- tabPanel("ISLM",
                     mainPanel(
                      hr(),
-                     
+                     tags$head(
+                       tags$style(type="text/css", "label{ display: table-cell; text-align: center; vertical-align: middle; } .form-group { display: table-row;}")
+                     ),
                      fluidRow(
-                       # label next to input 
-                       tags$head(
-                         tags$style(type="text/css", "label{ display: table-cell; text-align: center; vertical-align: middle; } .form-group { display: table-row;}")
-                       ),
-                       column(3,
+                       column(6,
+                              h4("Goods Market"),
+                              plotOutput("goodsMarketPlot")),
+                       column(2,
                               h4("Parameters ISLM"),
                               p("Define parameters of the ISLM model and change 
                                 endogenous and exogenous paramters to see how 
-                                this changes equilibrium output.")
+                                those changes affect equilibrium output.")
                        ),
-                       column(4, offset = 1,
+                       column(2,
                               h4("Model Parameters"),
                               numericInput("c_0_islm", label = withMathJax("$$c_0 = $$"), 
-                                           value = 150, min = 50, max = 500, step = 10),
-                              numericInput("c_1_islm", label = withMathJax("$$c_1 = $$"), 
-                                           value = 0.5, min = 0.01, max = 0.99, step = 0.1)
-                       ),
-                       column(4,
-                              h4("Decision Variables"),
-                              p("Parameters the government (G and T) and the Central Bank (i) can influence."),
-                              numericInput("G_islm", label = withMathJax("$$G = $$"), 
                                            value = 200, min = 100, max = 500, step = 10),
+                              numericInput("c_1_islm", label = withMathJax("$$c_1 = $$"), 
+                                           value = 0.5, min = 0.01, max = 0.99, step = 0.1),
+                              numericInput("I_0_islm", label = withMathJax("$$I_0 = $$"), 
+                                           value = 150, min = 0, max = 500, step = 10),
+                              numericInput("I_1_islm", label = withMathJax("$$I_1 = $$"), 
+                                           value = 0.2, min = 0.01, max = 0.99, step = 0.1),
+                              numericInput("I_2_islm", label = withMathJax("$$I_2 = $$"), 
+                                           value = 1000, min = 0, max = 10000, step = 100)
+                       ),
+                       column(2,
+                              h4("Decision Variables"),
+                              p("Parameters the government (G and T) and the Central Bank (i) can directly influence."),
+                              numericInput("G_islm", label = withMathJax("$$G = $$"), 
+                                           value = 200, min = 0, max = 500, step = 10),
                               numericInput("T_islm", label = withMathJax("$$T = $$"), 
                                            value = 100, min = 0, max = 500, step = 10),
                               numericInput("i_islm", label = withMathJax("$$i = $$"), 
-                                           value = 0.02, min = -0.01, max = 0.2, step = 0.01)
+                                           value = 0.02, min = -0.01, max = 0.5, step = 0.01)
                        )
                      ),
-                     plotOutput("islmPlot"),
+                     fluidRow(
+                       column(6,
+                              h4("IS and LM Curve"),
+                              plotOutput("islmPlot")),
+                       column(6, 
+                              h4("LM Curve"),
+                              plotOutput("interestRatePlot"))
+                     ),
+                     #plotOutput("islmPlot"),
                      textOutput("islmEquilibrium")
                      )
 )
