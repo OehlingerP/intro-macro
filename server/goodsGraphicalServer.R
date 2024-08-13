b29a086c0d5aeef67fa907e106eea6ed61653e50
Peter Öhlinger <peteroehlinger@Peters-MacBook-Pro.local>
2024-08-13 22:31:28 +0200

    start with graphical representation of the goods market equilibria

--- server/goodsGraphicalServer.R
+++ server/goodsGraphicalServer.R
@@ -1,55 +1,78 @@
-output$plotPrivateConsumption <- renderPlotly({
+output$plotGoodsMarketEq <- renderPlotly({
   
-  plot <- ggplot()
+  plot_default <- plot <- gx_theme(ggplot(),
+                   x_title = "Income (Y)",
+                   y1_title = "Demand (Z), Production (Y)") +
+    theme(axis.text = element_blank(),
+          axis.text.x = element_blank()) +
+    scale_y_continuous(limits = c(0, 100), expand = c(0, 0)) +
+    scale_x_continuous(limits = c(0, 100), expand = c(0, 0))
   
-  plot <- gx_theme(ggplot(),
-                   x_title = "Disposable Income",
-                   y1_title = "Private Consumption") +
-    # theme(axis.text = element_blank()) +
-    scale_y_continuous(breaks = c(0), labels = c("0"), limits = c(0, 100), expand = c(0.01, 0)) +
-    scale_x_continuous(breaks = c(0), labels = c("0"), limits = c(0, 100), expand = c(0.01, 0))
-  
-  if(input$step2privateConsumption){
+  if(input$degree45Line){
     
-    plot <- plot +
-      geom_abline(slope = 0.5, intercept = 25)
-    
-  }
-  
-  if(input$step1privateConsumption){
-    
-    plot <- plot + 
-      geom_point(aes(x = 0, y = 25), color = "red", size = 2) 
+    plot <- plot_default +
+      geom_abline(slope = 1, intercept = 0)+
+      geom_curve(aes(x = 10, y = 0, xend = 5, yend = 5),
+                 curvature = 0.3) +
+      annotate("text", x = 12.5, y = 3.5, label = "45°", size = 4)
     
     plot <- ggplotly(plot) %>%
       layout(
         annotations = list(
           list(
-            x = 0,
-            y = 25,
-            text = "c<sub>0</sub>",  # Math expression in plotly format
+            x = 75,
+            y = 75,
+            text = "Slope = 1",  # Math expression in plotly format
             showarrow = TRUE,
-            arrowhead = 2
+            arrowhead = 2,
+            ax = -30,  # Arrow line start x-offset relative to the annotation
+            ay = -30   # Arrow line start y-offset relative to the annotation
           )
         )
       )
     
   }
   
-  if(input$step2privateConsumption){
+  if(input$zzLine){
     
-    plot <- ggplotly(plot)  %>%
+    plot <- plot_default +
+      geom_abline(slope = 1, intercept = 0)+
+      geom_abline(slope = 0.4, intercept = 30, color = gx_colors()[2]) +
+      geom_point(aes(x = 50, y = 50), size = 2)
+
+    
+    plot <- ggplotly(plot) %>%
       layout(
         annotations = list(
           list(
+            x = 75,
+            y = 75,
+            text = "Production Y<br>Slope = 1",  # Math expression in plotly format
+            showarrow = TRUE,
+            arrowhead = 2,
+            ax = -40,  # Arrow line start x-offset relative to the annotation
+            ay = -40   # Arrow line start y-offset relative to the annotation
+          ),
+          list(
             x = 50,
             y = 50,
-            text = "slope = c<sub>1</sub>",  # Math expression in plotly format
+            text = "Equilibrium:<br>Y = Z",  # Math expression in plotly format
             showarrow = TRUE,
-            arrowhead = 2
-          )
+            arrowhead = 2,
+            ax = 40,  # Arrow line start x-offset relative to the annotation
+            ay = 40   # Arrow line start y-offset relative to the annotation
+          ),
+          list(
+              x = 30,
+              y = 42,
+              text = "Demand ZZ<br>Slope = c<sub>1</sub>", 
+              showarrow = TRUE,
+              arrowhead = 2,
+              ax = -40,  # Arrow line start x-offset relative to the annotation
+              ay = -40   # Arrow line start y-offset relative to the annotation
         )
       )
+    )
     
   }
   
@@ -57,28 +80,28 @@ output$plotPrivateConsumption <- renderPlotly({
   
 })
 
-output$textStep1ConsumptionPlot <- renderText({
+output$textStep1GoodsMarketEqPlot <- renderText({
   
-  if(input$step1privateConsumption){
+  if(input$degree45Line){
     
-    "\\(c_0\\) represents the essential level of consumption required for survival.
-    Even in the absence of income, consumption cannot be zero because basic needs must be 
-    met. Therefore, \\(c_0>0\\) signifies that there is a minimum amount of consumption 
-    necessary to sustain life, regardless of income."
+    "We draw production as a function of income which is simply a straight line
+    with slope 1."
     
   }
   
 })
 
-output$textStep2ConsumptionPlot <- renderText({
+output$textStep2GoodsMarketEqPlot <- renderText({
   
-  if(input$step2privateConsumption){
+  if(input$zzLine){
     
-    "\\(c_1\\) represents the share of income spent on consumption. For simplicity we
-    assume that this rate is constant. We will also assume that \\(0 < c_1 < 1\\). This
-    seems to be a reasonable assumption even though for some individuals this may not be
-    true."
+    "We can split our aggregate demand function \\(Z = c_0+I+G-c_1T + c_1Y\\)
+    into two parts: \\(c_0+I+G-c_1T\\) refers to autonomous consumption, i.e. 
+    consumption that does not depend on income. This reflects the intercept. The
+    second part \\(c_1Y\\) describes how demand changes with income and, thus 
+    describes the slope of the function."
     
   }
   
 })
+
--- ui/goodsGraphicalUI.R
+++ ui/goodsGraphicalUI.R
@@ -7,6 +7,39 @@
 goodsGraphicalTab <- tabPanel("Graphical Equilibrium", 
                                withMathJax(),
                                mainPanel(
-                                
+                                p("We will now solve for the equilibrium output graphically. This 
+                                  approach will enhance our understanding of the equilibrium concept 
+                                  and also illustrate how the multiplier effect operates in practice. 
+                                  By visualizing the equilibrium output on a graph, we can see how 
+                                  changes in spending influence overall economic activity and how the 
+                                  multiplier amplifies these effects."),
+                                p("We will explore this concept through a step-by-step exercise. Take your 
+                                  time to think through each step before checking the solution. If your initial 
+                                  attempt doesn't match the expected result, carefully review where you might 
+                                  have gone wrong to improve your understanding."),
+                                fluidRow(
+                                  column(8, 
+                                         plotlyOutput("plotGoodsMarketEq")),
+                                  column(4, 
+                                         p("On the left, you see a graph with income on the horizontal 
+                                           axis and demand/output on the vertical axis. Demand and output 
+                                           are on one axis because in equilibrium the two are the same."),
+                                         p("Step 1: Draw the line representing all points of equilibrium, 
+                                           where income equals production."),
+                                         awesomeCheckbox(
+                                           inputId = "degree45Line",
+                                           label = "Show solution"
+                                         ),
+                                         textOutput("textStep1GoodsMarketEqPlot"),
+                                         br(),
+                                         p("Step 2: Draw the demand function and find equilibrium output. Why must the 
+                                           two lines cross?"),
+                                         awesomeCheckbox(
+                                           inputId = "zzLine",
+                                           label = "Show solution"
+                                         ),
+                                         textOutput("textStep2GoodsMarketEqPlot"),
+                                         br())
+                                )
                                )
 )
